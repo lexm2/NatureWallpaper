417df8d1801b1fb0cc1b867acc6953899108c572
lexm2 <91219911+lexm2@users.noreply.github.com>
2023-08-27 22:36:43 -0400

    deleted stuff

--- src/js/apiClient.js
+++ src/js/apiClient.js
@@ -1,20 +1,26 @@
-import { createClient } from "./pexels";
 
-const client = createClient(
-  "6fobEFX4FuOFIpuNpk5RNUBFffdoOU0BV8qozv6KxZBAfPI74GEgE1xD"
-); //dont touch this ever
-
-const query = "Nature";
-const apiUrl = "https://api.pexels.com/v1/search";
-const orientation = "landscape";
-const perPage = 10;
-
-client.videos
-  .search({ query, orientation, per_page: perPage })
-  .then((response) => {
-    console.log(JSON.stringify(response));
-    console.log(response.videos);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+  const apiKey = "6fobEFX4FuOFIpuNpk5RNUBFffdoOU0BV8qozv6KxZBAfPI74GEgE1xD"; //dont touch this ever if you are not lex
+  const apiUrl = 'https://api.pexels.com/v1/search?query=nature&per_page=10';
+  
+  async function fetchData() {
+      try {
+          const response = await fetch(apiUrl, {
+              headers: {
+                  Authorization: apiKey,
+              },
+          });
+  
+          if (!response.ok) {
+              throw new Error('Request failed');
+          }
+  
+          const data = await response.json();
+  
+          // Display the fetched data
+          console.log(JSON.stringify(data, null, 2));
+      } catch (error) {
+          console.error('Error fetching data:', error);
+          dataContainer.innerHTML = "<p>Error fetching data.</p>";
+      }
+  }
+  
